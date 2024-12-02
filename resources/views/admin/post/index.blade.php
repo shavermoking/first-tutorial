57d11f668289e35a1f31c00c94adcf79860b6f75
Никита Чуркин <shavermoking@gmail.com>
2024-12-02 22:45:21 +0300

    added admin

---
+++ resources/views/admin/post/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div>
+        <a href="{{ route('post.create') }}" class="btn btn-success mb-3">Create Post</a>
+    </div>
+
+    @foreach($posts as $post)
+        <div><a href="{{ route('post.show', $post->id) }}">{{ $post->id }} . {{ $post->title }}</a></div>
+        <br>
+    @endforeach
+
+    <div class="mt-3">
+        {{ $posts->withQueryString()->links() }}
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,11 @@ Route::group(['namespace' => 'Post'], function () {
     Route::delete('/posts/{post}', 'DestroyController')->name('post.delete');
 });
 
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Post'], function () {
+        Route::get('/post', 'IndexController')->name('admin.post.index');
+    });
+});
 
 
 Route::get('/main', 'MainController@index')->name('main.index');
