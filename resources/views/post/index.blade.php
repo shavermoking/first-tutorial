462a232dd6f70c417da80c96f4f1455bf7315464
Никита Чуркин <shavermoking@gmail.com>
2024-12-02 20:45:06 +0300

    added Pagination

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -8,4 +8,8 @@
             <div><a href="{{ route('post.show', $post->id) }}">{{ $post->id }} . {{ $post->title }}</a></div>
              <br>
         @endforeach
+
+    <div class="mt-3">
+        {{ $posts->links() }}
+    </div>
 @endsection
