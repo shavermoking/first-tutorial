413d4ba46fafded68be2174b8533b6c304178ec6
Никита Чуркин <shavermoking@gmail.com>
2024-12-05 13:20:28 +0300

    added Excel import

--- README.md
+++ README.md
@@ -15,3 +15,4 @@
 
 - JWT
 - Guzzle
+- Laravel Excel
---
+++ app/Console/Commands/ImportExcelCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Components\ImportDataClient;
+use App\Imports\PostsImport;
+use App\Models\Post;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Console\Command;
+use Maatwebsite\Excel\Facades\Excel;
+
+class ImportExcelCommand extends Command
+{
+    protected $signature = 'import:excel';
+
+    protected $description = 'Command description';
+
+
+    /**
+     * @throws GuzzleException
+     */
+    public function handle()
+    {
+        ini_set('memory_limit', '-1');
+        Excel::import(new PostsImport(), public_path('excel/posts.xlsx'));
+
+        dd('finish');
+    }
+}
