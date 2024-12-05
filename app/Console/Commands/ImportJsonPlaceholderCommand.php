dfc751ddbd948faf7180a5cce2b33ac33511bd71
Никита Чуркин <shavermoking@gmail.com>
2024-12-05 12:29:47 +0300

    added Guzzle usings

---
+++ app/Console/Commands/ImportJsonPlaceholderCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Components\ImportDataClient;
+use App\Models\Post;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Console\Command;
+
+class ImportJsonPlaceholderCommand extends Command
+{
+    protected $signature = 'import:jsonplaceholder';
+
+    protected $description = 'Command description';
+
+
+    /**
+     * @throws GuzzleException
+     */
+    public function handle()
+    {
+        $importClass = new ImportDataClient();
+
+        $response = $importClass->client->request('GET', '/posts');
+        $data = json_decode($response->getBody()->getContents());
+
+        foreach ($data as $datum){
+            Post::query()->firstOrCreate([
+                'title' => $datum->title
+            ],
+            [
+                'title' => $datum->title,
+                'content' => $datum->body,
+                'category_id' => 1
+            ]);
+        }
+
+        dd('finish');
+    }
+}
